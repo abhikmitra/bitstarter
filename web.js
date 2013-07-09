6e174d65e85f6bdbde21a271b8386f1ee7b3616b
abhikmitra <abhikprince@gmail.com>
2013-07-09 17:29:35 +0000

    Added buffer

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+
+var txt=fs.readFile("index.html");
+var buf=new Buffer(txt);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
