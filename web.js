359f9c1e5680d12a68cb65f3a2faad1284502ad4
abhikmitra <abhikprince@gmail.com>
2013-07-09 17:50:18 +0000

    reverting

--- web.js
+++ web.js
@@ -1,15 +1,13 @@
 var express = require('express');
 var fs = require('fs');
 
-var txt=fs.readFile("index.html");
+var txt=fs.readFileSync("index.html");
 var buf=new Buffer(txt);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString());
-});
-
+  response.send("hello");
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
