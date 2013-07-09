15a190fef45e0e81fc305674b789ab64d683e256
abhikmitra <abhikprince@gmail.com>
2013-07-09 18:04:25 +0000

    reverting123

--- web.js
+++ web.js
@@ -7,6 +7,7 @@ var buf=new Buffer(txt);
 
 app.get('/', function(request, response) {
   response.send("hello");
+});
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
