d035847526f22f997e39b9d9ba049f47f56d04a8
abhikmitra <abhikprince@gmail.com>
2013-07-17 11:36:03 +0000

    updated index

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -45,6 +46,33 @@ var loadChecks = function(checksfile) {
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
+    
+    $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+var checkURL = function(url, checksfile) {
+    rest.get(apiurl).on('complete', function(result, response){
+	if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+             $ = cheerioHtmlFile(result);
+                var checks = loadChecks(checksfile).sort();
+                var out = {};
+                for(var ii in checks) {
+		            var present = $(checks[ii]).length > 0;
+		            out[checks[ii]] = present;
+		        }
+                var outJson = JSON.stringify(out, null, 4);
+                console.log(outJson);
+        }
+	
+	});
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
@@ -65,7 +93,13 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL of the site', clone(assertFileExists), "http://www.google.com")
         .parse(process.argv);
+    if(program.url!="" && program.url!=null)
+    {
+	
+    }
+    else
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
